3961290b888872ce025b8fa89548b3983070148f
Keziah Gertrude Rosales <krosales.intern.integratr@outlook.com>
2023-04-20 10:35:57 +0800

    typography

---
+++ src/components/TheTypography.vue
@@ -0,0 +1,199 @@
+<template>
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poppins</title>
+</head>
+    <body>
+
+    <div class="container">
+      <p class="poppins">Poppins</p>
+    </div> 
+
+    <!-- Heading 1 -->
+
+    <div class="container"><hr>
+      <div class="row align-items-start">
+        <div class=" col ">
+           <p class="Poppins-H1-SemiBold"> Display 01 Semi Bold</p>
+        </div>
+        <div class="col">
+           <p class="Poppins-H1-Bold"> Display 01 Bold </p>
+        </div>
+        <div class="col">
+          <p class="Poppins-H1-Black">Display 01 Black </p>
+       </div>
+      </div>
+    </div>
+   
+    <!-- Heading 2 -->
+
+    <div class="container"><hr>
+      <div class="row align-items-start">
+       <div class=" col ">
+        <p class="Poppins-H2-SemiBold">Display 02 Semi Bold</p>
+       </div>
+       <div class="col">
+          <p class="Poppins-H2-Bold"> Display 02 Bold </p>
+      </div>
+        <div class="col">
+        <p class="Poppins-H2-Black">Display 02 Black </p>
+        </div>
+     </div>  
+    </div>
+
+     <!-- Subheading -->
+
+    <div class="container"><hr>
+      <div class="row align-items-start">
+       <div class=" col ">
+        <p class="Poppins-SubHeading-SemiBold">Display 03 Semi Bold</p>
+       </div>
+       <div class="col">
+        <p class="Poppins-SubHeading-Bold"> Display 03 Bold </p>
+       </div>
+      <div class="col">
+        <p class="Poppins-SubHeading-Black">Display 03 Black </p>
+      </div>
+    </div>
+    </div>
+
+  <!-- Body 1 -->
+
+    <div class="container"><hr>
+      <div class="row align-items-start">
+       <div class=" col ">
+         <p class="Poppins-Body-Light"> Display 04 Light</p>
+       </div>
+       <div class="col">
+        <p class="Poppins-Body-Regular"> Display 04 Regular </p>
+       </div>
+       <div class="col">
+        <p class="Poppins-Body-SemiBold">Display 04 Semi Bold </p>
+       </div>
+      </div>
+    </div>  
+     
+  <!-- Body 2 -->
+
+    <div class="container"><hr>
+      <div class="row align-items-start">
+       <div class=" col ">
+         <p class="Poppins-Body2-Light"> Display 05 Light</p>
+       </div>
+       <div class="col">
+         <p class="Poppins-Body2-Regular"> Display Regular </p>
+       </div>
+       <div class="col">
+         <p class="Poppins-Body2-SemiBold">Display Semi Bold</p>
+       </div>
+      </div>
+    </div> <br>
+
+  <!-- INTER -->
+
+    <div class="container"><br>
+      <p class="inter">Inter</p>
+    </div>
+
+    <!-- Heading 1 -->
+    <div class="container"><hr>
+      <div class="row align-items-start">
+        <div class=" col ">
+           <p class="Inter-H1-SemiBold"> Display 01 Semi Bold</p>
+        </div>
+        <div class="col">
+           <p class="Inter-H1-Bold"> Display 01 Bold </p>
+        </div>
+        <div class="col">
+          <p class="Inter-H1-Black">Display 01 Black </p>
+       </div>
+      </div>
+    </div>
+   
+    <!-- Heading 2 -->
+
+    <div class="container"><hr>
+      <div class="row align-items-start">
+       <div class=" col ">
+        <p class="Inter-H2-SemiBold">Display 02 Semi Bold</p>
+       </div>
+       <div class="col">
+          <p class="Inter-H2-Bold"> Display 02 Bold </p>
+      </div>
+        <div class="col">
+        <p class="Inter-H2-Black">Display 02 Black </p>
+        </div>
+     </div>  
+    </div>
+
+ <!-- Subheading -->
+
+    <div class="container"><hr>
+      <div class="row align-items-start">
+       <div class=" col ">
+        <p class="Inter-SubHeading-SemiBold">Display 03 Semi Bold</p>
+       </div>
+       <div class="col">
+        <p class="Inter-SubHeading-Bold"> Display 03 Bold </p>
+       </div>
+      <div class="col">
+        <p class="Inter-SubHeading-Black">Display 03 Black </p>
+      </div>
+    </div>
+    </div>
+
+  <!-- Body 1 -->
+
+    <div class="container"><hr>
+      <div class="row align-items-start">
+       <div class=" col ">
+         <p class="Inter-Body-Light"> Display 04 Light</p>
+       </div>
+       <div class="col">
+        <p class="Inter-Body-Regular"> Display 04 Regular </p>
+       </div>
+       <div class="col">
+        <p class="Inter-Body-SemiBold">Display 04 Semi Bold </p>
+       </div>
+      </div>
+    </div>  
+     
+  <!-- Body 2 -->
+
+    <div class="container"><hr>
+      <div class="row align-items-start">
+       <div class=" col ">
+         <p class="Inter-Body2-Light"> Display 05 Light</p>
+       </div>
+       <div class="col">
+         <p class="Inter-Body2-Regular"> Display Regular </p>
+       </div>
+       <div class="col">
+         <p class="Inter-Body2-SemiBold">Display Semi Bold</p>
+       </div>
+      </div>
+    </div> 
+
+    <!-- Body 3 -->
+    <div class="container"><hr>
+      <div class="row align-items-start">
+       <div class=" col ">
+         <p class="Inter-Body3-Light"> Display 06 Light</p>
+       </div>
+       <div class="col">
+         <p class="Inter-Body3-Regular"> Display 06 Regular </p>
+       </div>
+       <div class="col">
+         <p class="Inter-Body3-SemiBold">Display 06 Semi Bold</p>
+       </div>
+      </div>
+    </div> 
+
+  </body>
+</html>
+</template>
+
