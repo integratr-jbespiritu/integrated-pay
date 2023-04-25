ee2247ed7aa042ec1e8b942c3e00eda85ea64686
Melissa F. Baroro <mbaroro.intern.integratr@outlook.com>
2023-04-25 13:32:39 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="header">
+    <img src="src/assets/Logo_White.svg" alt="Logo">
+   <div class="header3">
+      <img src="src/assets/notif.svg" alt="notif">
+    </div>
+    <i class="isax isax-menu-1"></i>
+    </div>
+  </template>
+  
+  <style>
+  .header {
+      display: flex;
+      width: 1536px;
+      height: 64px;
+      left: 0px;
+      top: 0px;
+      background: #000000;
+  }
+
+  .header img {
+      width: 132px;
+      /* height: 36px; */
+      max-width: 100%;
+      margin-left: 1rem;
+  }
+
+  .header2 img {
+    width: 30px;
+    max-width: 100%;
+    margin-left: 18rem;
+    padding-block: 24px;
+    
+  }
+
+  .header3 img {
+  width: 27px;
+  margin-left: 70rem;
+ }
+
+  .isax {
+    margin-left: 10rem;
+    width: 90px;
+    padding: 90px;
+  }
+
+ 
+  </style>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
+import Header from '../components/Header.vue'
 import Table from '../components/Table.vue'
 </script>
 
 <template>
   <main>
-    <Table />
+    <Header /> 
+   <!-- <Table /> -->  
   </main>
 </template>
