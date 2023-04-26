0c79786bb5b700aa2396e735c7e4ed7c63296868
Melissa F. Baroro <mbaroro.intern.integratr@outlook.com>
2023-04-26 09:44:04 +0800

    table commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Footer from '../components/Footer.vue'
 import TheTypography from '../components/TheTypography.vue'
+import Table from '../components/Table.vue';
 </script>
 
 <template>
   <main>  
     <TheTypography />
     <Footer />
+    <Table/>
   </main>
 </template>
