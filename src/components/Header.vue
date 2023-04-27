02860764f0e855b3d40aecda0552f43ab4983a91
Melissa F. Baroro <mbaroro.intern.integratr@outlook.com>
2023-04-27 10:10:30 +0800

    2nd commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
         <i class="isax isax-search-status-1"></i>
     </div>
     <div class="icon2">
-        <i class="isax isax-notification"></i>
+        <i class="isax isax-notification5"></i>
     </div>
 </div>
   </template>
