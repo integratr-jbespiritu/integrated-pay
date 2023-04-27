41483100f1214fec5a779f18e8d0c5f44f403c18
Melissa F. Baroro <mbaroro.intern.integratr@outlook.com>
2023-04-27 12:04:13 +0800

    commit

---
+++ src/components/Payment.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="con">
+        <div class="row ">
+            <div class="pd col-md-4">Payment Details</div>
+            <div class="rfd col-md-4 offset-md-4">Refund</div>
+            <hr style="width:98%;text-align:left;margin-left:1%">
+        </div>
+    </div>
+    <div class="container1">
+        <div class="row">
+            <div class="rfnd col-md-4">Refund Details</div>
+            <div class="rfnd1 col-md-4 offset-md-4">Refund</div>
+            <hr style="width:98%;text-align:left;margin-left:1%">
+        </div>
+    </div> 
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,18 @@
 import Footer from '../components/Footer.vue'
 import TheTypography from '../components/TheTypography.vue'
 import Header from '../components/Header.vue'
+import Payment from '../components/Payment.vue'
+import IssueRefund from '../components/IssueRefund.vue'
+import Table from '../components/Table.vue'
 </script>
 
 <template>
   <main>  
-    <Header />
-    <TheTypography />
-    <Footer />
+  <!-- <Header /> -->
+  <!-- <TheTypography /> -->
+  <!-- <Footer />  -->
+  <!-- <Payment /> -->
+  <!-- <Table /> -->
+  <IssueRefund /> 
   </main>
 </template>
