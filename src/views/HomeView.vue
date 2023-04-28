436a4775cc8384a99244aea7e2c798da5a62b773
Melissa F. Baroro <mbaroro.intern.integratr@outlook.com>
2023-04-28 13:18:31 +0800

    fix conflict

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,17 @@
 <script setup>
-import Footer from '../components/Footer.vue'
+import Header from '../components/Header.vue';
 import TheTypography from '../components/TheTypography.vue'
 import Table from '../components/Table.vue';
+import Footer from '../components/Footer.vue'
+
+
 </script>
 
 <template>
   <main>  
+    <Header />
     <TheTypography />
+    <Table />
     <Footer />
-    <Table/>
   </main>
 </template>
