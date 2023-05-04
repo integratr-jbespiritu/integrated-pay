14596902baa7698325fc059d7ea4b893f30a83be
Melissa F. Baroro <mbaroro.intern.integratr@outlook.com>
2023-05-04 13:14:22 +0800

    fixed header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,10 @@
 <template>
-    <div class="container">
-<div class="header">
-    <div>
-    <img src="src/assets/images/Logo_White.svg" alt="Logo">
-    </div>  
-
-    <i class="isax isax-search-status-1 icon-search position-absolute top-50 start-50 translate-middle"></i>  
-    <i class="isax isax-notification5 icon-notification position-absolute top-50 end-0 translate-middle"></i>
-
-</div>
-</div>
-  </template>
+  <div class="header">
+      <header class="d-flex justify-content-center py-2">
+          <div class="navbar-brand col-md-3 col-lg-2 me-0 px-1">
+          </div>
+          <i class="isax isax-search-status-1 icon-search position-absolute top-50 translate-middle ms-1 py-5"></i>
+          <i class="isax isax-notification5 icon-notification5 position-absolute top-50 translate-middle"></i>
+      </header>
+  </div>
+</template>
