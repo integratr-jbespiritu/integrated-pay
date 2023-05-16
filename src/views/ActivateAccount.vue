b355ea82e40215c1536472c2e0fffed14e85d98a
integratr-intern-krosales <krosales.intern.integratr@outlook.com>
2023-05-16 09:14:21 +0800

    fixed indention and class name

--- src/views/ActivateAccount.vue
+++ src/views/ActivateAccount.vue
@@ -1,116 +1,114 @@
 <template>
   <Header/>
   <Sidebar/>
-    <main class="container d-flex col-md-9 col-lg-10 " style="margin-left: 12rem;">
-      <div class="container ms-5 mt-3 pb-5" >
-
-      <div class="row justify-content-center mb-3 mt-5 ">
-			<div class="col-8 text-center p-0 mb-2 ps-1 pe-1 ms-4">
-				<div class="px-0 pb-0 m t-3 mb-3">
-          <form id="form">
-            <div class="progressbar">
-              <div class="steps1"> <span class="check mt-2"></span>
-              </div> 
-              <a class="step2-circle" title="General Information" data-bs-toggle="tooltip" data-bs-placement="top">
-                <div class="inner-circle position-absolute top-50 start-50 translate-middle"></div>
-              </a>
-              <div class="step3-circle">
-                <div class="step3-inner-circle position-absolute top-50 start-50 translate-middle"></div>
-              </div>
-              <div class="steps4"></div>
+<main class="container gx-2 g-0 d-flex" style="margin-left: 12rem;">
+   <div class="container ms-5 mt-3 pb-5" >
+        <div class="container">
+          <div class="row gx-0 g-0 justify-content-center mb-3 mt-5 ">
+		      	<div class="col-9 text-center p-0 mb-2 ps-1 ms-5">
+			      	<div class="px-0 pb-0 mt-3 mb-3">
+                 <form id="form">
+                    <div class="progressbar">
+                      <div class="step1"> <span><img src="/src/assets/images/check.svg" alt=""> </span> </div>
+                         <a class="step2-circle" title="General Information" data-bs-toggle="tooltip" data-bs-placement="top">
+                            <div class="inner-circle position-absolute top-50 start-50 translate-middle"></div>
+                         </a>
+                       <div class="step3-circle">
+                            <div class="step3-inner-circle position-absolute top-50 start-50 translate-middle"></div>
+                      </div>
+                            <div class="step4"></div>
+                     </div>
+                  </form>
+                </div>  
             </div>
-          </form>
-        </div>  
-      </div>
-      </div>
+          </div>
         
-    <div class="container bg-white pb-2 ms-4">
-    <div class="py-2 text-center">
-      <h1 class="inter-h1-black fw-bold py-2" style="font-size: 32px; color: var(--vt-c-black);">
-         General Information</h1>
-      <div class="border-bottom "></div>
-    </div>
+    <div class="container p-3 bg-white pb-3 ms-4">
+      <div class="py-1 text-center">
+           <h1 class="inter-h1-black fw-bold mb-3" style="font-size: 32px; color: var(--vt-c-black);">
+            General Information</h1>
+          <div class="border-bottom mb-2"></div>
+     </div>
 
   <div class="container row gx-0 g-0 pt-2">
     <div class="col-12">
       <div class="row gx-2 g-0">
-
         <div class="col-12 col-md-6 col-lg-6">
-          <!-- input first name -->
-          <div class="input-group mb-3 px-1">
+            <!-- input first name -->
+            <div class="input-group mb-3 px-1">
               <form class="form-floating">
-                <input type="text" class="input-height form-control" id="floatingInputValue" placeholder="name@example.com"  value="Juan">
-                <label for="floatingInputValue">First Name</label>
+                  <input type="text" class="input-height form-control" id="floatingInputValue" placeholder="name@example.com"  value="Juan">
+                  <label for="floatingInputValue">First Name</label>
               </form>
-          </div>
+            </div>
         </div>
 
         <div class="col-12 col-md-6 col-lg-6">
               <!-- input field lastname  -->
-              <div class="input-group mb-3 px-1">
-            <form class="form-floating">
-              <input type="email" class="input-height form-control" id="floatingInputValue" placeholder="Lastname">
-              <label for="floatingInputValue">Last Name</label>
-            </form>
-          </div>  
+            <div class="input-group mb-3 px-1">
+               <form class="form-floating">
+                 <input type="email" class="input-height form-control" id="floatingInputValue" placeholder="Lastname">
+                 <label for="floatingInputValue">Last Name</label>
+              </form>
+            </div>  
         </div>
 
         <div class="col-12 col-md-6 col-lg-6">
               <!-- input field email -->
-              <div class="input-group mb-3 px-1">
-            <form class="form-floating">
-              <input type="email" class="input-height form-control" id="floatingInputValue" placeholder="Email">
-              <label for="floatingInputValue">Email</label>
-            </form>
-          </div>
+            <div class="input-group mb-3 px-1">
+              <form class="form-floating">
+                  <input type="email" class="input-height form-control" id="floatingInputValue" placeholder="Email">
+                  <label for="floatingInputValue">Email</label>
+              </form>
+            </div>
         </div>
 
         <div class="col-12 col-md-6 col-lg-6">
             <!-- input mobile number -->
             <div class="input-group mb-3 px-1">
-            <form class="form-floating">
-              <input type="email" class="input-height form-control" id="floatingInputValue" placeholder="Mobile Number">
-              <label for="floatingInputValue">Mobile Number</label>
+              <form class="form-floating">
+                <input type="email" class="input-height form-control" id="floatingInputValue" placeholder="Mobile Number">
+                <label for="floatingInputValue">Mobile Number</label>
             </form>
           </div>
         </div>
 
         <div class="col-12 col-md-6 col-lg-6">
               <!-- input field date of birth -->
-              <div class="input-group mb-3 px-1">
-            <form style="width: 100% ">
-              <input type="text" class="input-height form-control" placeholder="Date of Birth" id="exampleInputEmail1" aria-describedby="emailHelp">
-              <i class="icon isax isax-calendar-1 password-toggle"></i>
+            <div class="input-group mb-3 px-1">
+               <form style="width: 100% ">
+                  <input type="text" class="input-height form-control" placeholder="Date of Birth" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  <i class="icon-activate isax isax-calendar-1 password-toggle"></i>
              </form>
-          </div>
+            </div>
         </div>
         
         <div class="col-12 col-md-6 col-lg-6">
                 <!-- input nationality -->
-                <div class="input-group mb-3 px-1">
-            <form class="form-floating">
-              <input type="email" class="input-height form-control" id="floatingInputValue" placeholder="Nationality">
-              <label for="floatingInputValue">Nationality</label>
-            </form>
-          </div>
+            <div class="input-group mb-3 px-1">
+              <form class="form-floating">
+                  <input type="email" class="input-height form-control" id="floatingInputValue" placeholder="Nationality">
+                  <label for="floatingInputValue">Nationality</label>
+              </form>
+            </div>
         </div>
 
         <div class="col-12 col-md-6 col-lg-6">
                <!-- input country of birth -->
-               <div class="input-group mb-3 px-1">
-             <select class="input-height form-select" aria-label="Default select example">
-                <option selected>Country of Birth</option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-            </select>
-          </div>
+            <div class="input-group mb-3 px-1">
+               <select class="input-height form-select" aria-label="Default select example">
+                  <option selected>Country of Birth</option>
+                  <option value=""></option>
+                  <option value=""></option>
+                  <option value=""></option>
+              </select>
+            </div>
         </div>
 
         <div class="col-12 col-md-6 col-lg-6">
            <!-- input city of birth -->
            <div class="input-group mb-3 px-1">
-             <select class="input-city form-select" aria-label="Default select example">
+             <select class="input-height form-select" aria-label="Default select example">
                 <option selected>City of Birth</option>
                 <option value=""></option>
                 <option value=""></option>
@@ -119,21 +117,21 @@
           </div>
         </div>
       </div>
-      <p class=" col-12 inter-body-semibold text-start text-dark ms-2 mb-3" >Current Address</p>
+            <p class=" col-12 inter-body-semibold text-start text-dark ms-2 mb-3" >Current Address</p>
     <div class="row gx-2 g-0">
-      <div class="col-12 col-md-6 col-lg-6">
+       <div class="col-12 col-md-6 col-lg-6">
          <!-- housing address -->
          <div class="input-group mb-3 px-1">
             <form class="form-floating">
               <input type="email" class="input-height form-control" id="floatingInputValue" placeholder="House/Unit/Bldg.No">
               <label for="floatingInputValue">House/Unit/Bldg.No</label>
             </form>
+         </div>
         </div>
-      </div>
 
       <div class="col-12 col-md-6 col-lg-6">
              <!-- building name -->
-             <div class="input-group mb-3 px-1">
+          <div class="input-group mb-3 px-1">
             <form class="form-floating">
               <input type="email" class="input-height form-control" id="floatingInputValue" placeholder="Building Name">
               <label for="floatingInputValue">Building Name</label>
@@ -153,15 +151,15 @@
       
       <div class="col-12 col-md-6 col-lg-6">
              <!-- region -->
-             <div class="input-group mb-3 px-1">
-              <select class="input-height form-select" aria-label="Default select example">
+         <div class="input-group mb-3 px-1">
+            <select class="input-height form-select" aria-label="Default select example">
                 <option selected>Region</option>
                 <option value=""></option>
                 <option value=""></option>
                 <option value=""></option>
-              </select>
+            </select>
           </div>
-      </div>
+       </div>
       
       <div class="col-12 col-md-6 col-lg-6">
           <!-- province -->
@@ -171,21 +169,21 @@
                 <option value=""></option>
                 <option value=""></option>
                 <option value=""></option>
-            </select>
-          </div> 
-      </div>
+             </select>
+           </div> 
+       </div>
 
       <div class="col-12 col-md-6 col-lg-6">
               <!-- city / municipality -->
-            <div class="input-group mb-3 px-1">
+           <div class="input-group mb-3 px-1">
              <select class="input-height form-select" aria-label="Default select example">
                 <option selected>City or Municipality</option>
                 <option value=""></option>
                 <option value=""></option>
                 <option value=""></option>
             </select>
-          </div>
-      </div>
+           </div>
+       </div>
   
           <div class="col-12 col-md-6 col-lg-6">
                  <!-- baranggay -->
@@ -198,50 +196,47 @@
              </select>
           </div>
         </div>
+                <!-- Zipcode -->
           <div class="col-12 col-md-6 col-lg-6">
             <div class="input-group mb-3 px-1">
-            <form class="form-floating">
-              <input type="email" class="input-height form-control" id="floatingInputValue" placeholder="Zipcode">
-              <label for="floatingInputValue">Zip Code</label>
+             <form class="form-floating">
+                <input type="email" class="input-height form-control" id="floatingInputValue" placeholder="Zipcode">
+                <label for="floatingInputValue">Zip Code</label>
             </form>
             </div>
           </div>
     </div>
-        <!-- button -->
-  <div class="container py-1 mt-5">
-      <a class="position-absolute bottom-0 end-0 px-1 mb-2 text-decoration-none"  href="#"> 
-        <span class="px-3 text-purple">Back</span>
-        <router-link :to="{name: 'ActivateStepII'}">
-        <button type="button" class="click-hover-button">Continue</button> </router-link>
-      </a>
-  </div>
+                <!-- button -->
+          <div class="container py-2 mt-5">
+             <a class="position-absolute bottom-0 end-0 px-1 mb-2 text-decoration-none"  href="#"> 
+              <span class="px-3 text-purple">Back</span>
+              <router-link :to="{name: 'ActivateStepII'}">
+                <button type="button" class="activate-button">Continue</button> </router-link>
+              </a>
+            </div>
+        </div>
        </div>
       </div>
-      </div>
     </div>
+  </div>
 </main>
 </template>
 <script>
 
 import Header from '../components/Header.vue';
-import Footer from '../components/Footer.vue';
 import Sidebar from '../components/Sidebar.vue';
-
 
 export default {
   
-
-  name: "Login",
+  name: "LoginPage",
    components: {
       Header,
-      Footer,
       Sidebar
    } , mounted(){
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
    }
 };
-
 </script>
--- src/views/ActivateStepII.vue
+++ src/views/ActivateStepII.vue
@@ -1,178 +1,101 @@
 <template>
   <Header/>
   <Sidebar/>
-    <main class="container d-flex col-md-9 col-lg-10 " style="margin-left: 12rem;">
-      <div class="container ms-5 mt-3 pb-5" >
-
-      <div class="row justify-content-center mb-3 mt-5 ">
-			<div class="col-8 text-center p-0 mb-2 ps-1 pe-1 ms-4">
-				<div class="px-0 pb-0 m t-3 mb-3">
-          <form id="form">
-            <div class="progressbar">
-              <div class="steps1"> <span class="check mt-2"></span>
-              </div> 
-              <a class="step2-circle" title="General Information" data-bs-toggle="tooltip" data-bs-placement="top">
-                <div class="inner-circle position-absolute top-50 start-50 translate-middle"></div>
-              </a>
-              <div class="step3-circle">
-                <div class="step3-inner-circle position-absolute top-50 start-50 translate-middle"></div>
-              </div>
-              <div class="steps4"></div>
-            </div>
-          </form>
-        </div>  
-      </div>
-      </div>
-
-      <div class="container bg-white pb-2 ms-4">
-    <div class="py-2 text-center">
-      <h1 class="inter-h1-black fw-bold py-2" style="font-size: 32px; color: var(--vt-c-black);">
-         Are you a?...</h1>
-      <div class="border-bottom "></div>
-    </div>
-
-  <div class="container row gx-0 g-0 pt-2">
-    <div class="col-12">
-      <div class="row gx-0 g-0">
-
-        <div class="col-md-6">
-          <div class="gx-0 g-0">
-            <div class="container d-flex justify-content-center gx-0 g-0">
-              <div class="rectangle d-flex mt-4 mb-3 ms-3">
-              <div class="icons isax isax-shop4 ms-4 mx-3"></div>
-              <div class="container">
-                <div class="col">
-                    <h5 class=" title-6 text-start mt-3">Business</h5>
-                </div> 
-                  <div class="col mt-2">
-                  <p class="inter-regular me-3">Lorem ipsum dolor sit amet, consectetur adipiscing <br>
-                   elit, sed do eiusmod tempor incididunt ut labore et <br>
-                    dolore magna aliqua.</p>
-                </div>
-              </div>
-            </div> 
-            </div>  
-            <div class="container d-flex justify-content-center">
-              <div class="rectangle d-flex mt-2 mb-3 ms-3">
-              <div class="icons isax isax-bank ms-4 mx-3"></div>
-              <div class="container">
-                <div class="col">
-                    <h5 class=" title-6 text-start mt-3">Government Unit</h5>
-                </div> 
-                  <div class="col mt-2">
-                  <p class="inter-regular me-3">Lorem ipsum dolor sit amet, consectetur adipiscing <br>
-                   elit, sed do eiusmod tempor incididunt ut labore et <br>
-                    dolore magna aliqua.</p>
-                </div>
-             </div>
-          </div> 
-        </div>  
-      </div>
-    </div>
-       
-        <div class="col-md-6">
-            <div class="d-flex mt-4 mb-3 ">
-              <img src="src/assets/images/Step2.svg" class="activate-image ms-2">
-          </div>
-          </div>
-                <!-- button -->
-     <div class="mt-4 py-4 mb-3 me-5">
-      <a class="position-absolute bottom-0 end-0 px-0 text-decoration-none"  href="#"> 
-      <router-link :to="{name: 'Activate Account'}" class="text-decoration-none"><span class="px-3 pe-4 text-purple">Back</span></router-link>
-          <button type="button" class="click-hover-button me-2">Continue</button> </a>
-      </div> 
-        </div>
-      </div>
-  </div>
-  </div>
-
-<!-- 
-   <div class="container row gx-0 g-0 pt-2">
-        <div class="col-12 border"> -->
-          <!-- <div class="row gx-2 g-0">
-            <div class="col-12 col-md-6 col-lg-6 bg-info">
-              <div class="d-flex mt-4 mb-3x">
-              <div class="icons isax isax-shop4 ms-4 mt-0"></div> 
-              </div>
-            </div>
-              <div class="col-12 col-md-6 col-lg-6">
-                <img src="src/assets/images/Step2.svg" class="activate-image">
-              </div>
-            </div> -->
-          <!-- </div> -->
-     
-    
-  <!-- <div class="container pe-5 pb-3 ms-4 mt-4 bg-white">
-      <div class="py-2 text-center">
-        <h1 class="inter-h1-black fw-bold py-2" style="font-size: 32px; color: var(--vt-c-black);">
-           Are you a?...</h1>
-        <div class="border-bottom"> </div> 
-      </div> 
-    <div class="container pt-3">
-          <div class="row">
-           <div class="col-md-6">
-             <div class="rectangle d-flex mt-4 mb-3 ms-3">
-              <div class="icons isax isax-shop4 ms-4 mt-0"></div>
-    <div class="container">
-            <div class="col">
-                    <h5 class="inter-subheading-bold text-start text-black mt-3">Business</h5>
-            </div> 
-              <div class="col">
-                  <p class="inter-regular me-3">Lorem ipsum dolor sit amet, consectetur adipiscing <br>
-                   elit, sed do eiusmod tempor incididunt ut labore et <br>
-                    dolore magna aliqua.</p>
-                </div>
-              </div>
-            </div> 
-      <div class="rectangle d-flex mt-4 mb-3 ms-3">
-            <div class="icons isax isax-bank ms-4 mt-0"></div>
-              <div class="container">
-                <div class="col">
-                    <h5 class="fw-bold text-black mt-3">Government Unit</h5>
+  <main class="container gx-2 g-0 d-flex" style="margin-left: 12rem;">
+   <div class="container ms-5 mt-3 pb-5" >
+        <div class="container">
+          <div class="row gx-0 g-0 justify-content-center mb-3 mt-5 ">
+		      	<div class="col-9 text-center p-0 mb-2 ps-1 ms-5">
+			      	<div class="px-0 pb-0 mt-3 mb-3">
+                 <form id="form">
+                    <div class="progressbar">
+                      <div class="step1"> <span><img src="/src/assets/images/check.svg" alt=""> </span> </div>
+                         <a class="step2-circle" title="General Information" data-bs-toggle="tooltip" data-bs-placement="top">
+                            <div class="inner-circle position-absolute top-50 start-50 translate-middle"></div>
+                         </a>
+                       <div class="step3-circle">
+                            <div class="step3-inner-circle position-absolute top-50 start-50 translate-middle"></div>
+                      </div>
+                            <div class="step4"></div>
+                     </div>
+                  </form>
                 </div>  
-          <div class="col">
-                    <p class="inter-regular me-3">Lorem ipsum dolor sit amet, consectetur adipiscing <br>
-                   elit, sed do eiusmod tempor incididunt ut labore et <br>
-                    dolore magna aliqua.</p>
-                </div>
+            </div>
+          </div>
+
+      <div class="container p-3 bg-white pb-3 ms-4">
+         <div class="py-1 text-center">
+           <h1 class="inter-h1-black fw-bold mb-3" style="font-size: 32px; color: var(--vt-c-black);">
+              Are you a?...</h1>
+            <div class="border-bottom mb-2"></div>
+         </div>
+
+      <div class="container row gx-0 g-0 pt-2">
+        <div class="col-12">
+          <div class="row gx-0 g-0">
+             <div class="col-md-6">
+                <div class="gx-0 g-0">
+                  <div class="container d-flex justify-content-center gx-0 g-0">
+                    <div class="rectangle d-flex mt-4 mb-3 ms-3">
+                      <div class="icons isax isax-shop4 ms-4 mx-3"></div>
+                        <div class="container">
+                          <div class="col">
+                            <h5 class=" title-6 text-start mt-3">Business</h5>
+                          </div> 
+                            <div class="col mt-2">
+                          <p class="inter-regular me-3 text-break">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                            dolore magna aliqua.</p>
+                          </div>
+                        </div>
+                      </div> 
+                    </div>  
+                     <div class="container d-flex justify-content-center">
+                        <div class="rectangle d-flex mt-2 mb-3 ms-3">
+                          <div class="icons isax isax-bank ms-4 mx-3"></div>
+                            <div class="container">
+                               <div class="col">
+                                  <h5 class=" title-6 text-start mt-3">Government Unit</h5>
+                               </div> 
+                        <div class="col mt-2">
+                          <p class="inter-regular me-3 text-break">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                            dolore magna aliqua.</p>
+                        </div>
+                            </div>
+                          </div> 
+                    </div>  
+                 </div>
+              </div>
+       
+             <div class="col-md-6">
+                <div class="d-flex mt-4 mb-3 ">
+                  <img src="src/assets/images/Step2.svg" class="activate-image ms-2">
                 </div>
             </div>
-        </div>
-        <div class="col-md-6">
-          <div class="d-flex mt-4 mb-3 ">
-              <img src="src/assets/images/Step2.svg" class="activate-image">
+                <!-- button -->
+            <div class="mt-4 py-4 mb-3 me-5">
+               <a class="position-absolute bottom-0 end-0 px-0 text-decoration-none"  href="#"> 
+                <router-link :to="{name: 'Activate Account'}" class="text-decoration-none"><span class="px-3 pe-4 text-purple">Back</span></router-link>
+                <button type="button" class="activate-button me-2">Continue</button> </a>
+           </div> 
           </div>
-        </div> -->
-
-        <!-- button -->
-     <!-- <div class="mt-4 py-4 mb-4 me-5">
-      <a class="position-absolute bottom-0 end-0 px-0 text-decoration-none"  href="#"> 
-      <router-link :to="{name: 'Activate Account'}" class="text-decoration-none"><span class="px-3 pe-4 text-purple">Back</span></router-link>
-          <button type="button" class="click-hover-button me-4">Continue</button> </a>
-      </div>  -->
-
-          <!-- </div>
         </div>
-      </div> -->
+      </div>
+      </div>
     </div>
-    
-  </main> 
-  </template>
+  </div>
+</main> 
+</template>
     <script>
-  
-  import Header from '../components/Header.vue';
-  import Sidebar from '../components/Sidebar.vue';
-  import Footer from '../components/Footer.vue';
-  
+    import Sidebar from '../components/Sidebar.vue';
+
   export default {
-    name: "Login",
+    name: "LoginPage",
    components: {
-      Header,
-      Footer,
       Sidebar
    } , mounted(){
+    
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
    }
