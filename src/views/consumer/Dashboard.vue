7b715a5e04da4302154d5334eada6b94613fd060
Karen Bonganay <kbonganay.intern.integratr@outlook.com>
2023-06-23 18:49:22 +0800

    Activate Account

--- src/views/consumer/Dashboard.vue
+++ src/views/consumer/Dashboard.vue
@@ -3,12 +3,12 @@
       <div class="heading title-10 mx-3 my-2">
          Dashboard
       </div>
-      <div class="row gx-0 g-0 margin-x-20p">
+          <div v-if="active_account"  class="row gx-0 g-0 margin-x-20p"> <!--active_account-->
          <div class=" pe-4 ps-3 dash-image d-flex justify-content-between ">
             <div class="">
                <div class="justify-content-start align-items-start p-5 text-white">
                   <p class="title-11">Hello, Juan!</p>
-                  <p class="title-8">Welcome back to AmpliPAY, we’ve missed you.</p>
+                  <p class="title-24">Welcome back to Amplipay, we've missed you..</p>
                   <p class="title-12 text-start mt-1">Wednesday, Feb 23, 2023 3:40 PM PST</p>
                </div>
             </div>
@@ -17,6 +17,28 @@
             </div>
          </div>
       </div>
+
+      <div  v-if="!active_account"  class="row gx-0 g-0 margin-x-20p">
+         <div class=" pe-4 ps-3 dash-image d-flex justify-content-between ">
+            <div class="">
+               <div class="justify-content-start align-items-start p-5 text-white">
+                  <p class="title-11">Hello, Juan!</p>
+                  <p class="title-24">Complete the KYC process to activate your account and take full advantage of the features offered by
+                     AmpliPAY.</p>
+                  
+           <div class="col-12 col-md-4 d-flex pt-1">
+               <button type="button" class="button-hover-icon-purple bg-blue border-0 text-white">
+                <span class="title-8 translate-middle-y text-nowrap mx-1"> Activate Account </span></button>    
+          </div>
+                  <p class="title-12 text-start mt-1">Wednesday, Feb 23, 2023 3:40 PM PST</p>
+               </div>
+            </div>
+            <div class="mt-3">
+               <img src="/src/assets/images/character.svg" class="img-fluid d-none d-sm-none d-xl-block" alt="Overlay Image" />
+            </div>
+         </div>
+      </div>
+
       <div class="row gx-0 g-0 my-3 text-nowrap margin-x-20p">
          <div class="col-12 col-xl-6 pt-1 pb-1  mb-2 mb-xl-0">
             <div class="col bg-white me-0 me-xl-3">
@@ -28,6 +50,7 @@
                   Check the payout release schedule to know when you'll receive your funds.
                   </span>
                   </a>
+                  
                </div>
                <div class="calendar-date mx-5">
                   <a class="title-8 text-decoration-none text-blue title-11 no-hover"><i class="isax isax-calendar-1"></i>
@@ -81,7 +104,7 @@
                <div class="bg-white">
                   <div class="col-12 border-bottom py-2 title-4 d-flex justify-content-between">
                      <span class="title-6 mx-3">Net Revenue Income</span>
-                     <i class="isax isax-refresh text-purple fs-5 me-4" style="top: 5px;"></i>
+                         <img src="/src/assets/images/sync.svg" class="me-3">
                   </div>
                    <div class="d-flex mx-5 pt-3">
                      <p class="title-8"><span class="badge rounded-pill bg-purple">&nbsp;</span> GCash</p>
@@ -96,12 +119,25 @@
                      </div>
                      <div class="col-auto me-5">
                         <div class="d-flex">
-                           <p class="title-12 mt-4">February 2023&nbsp;</p>
+                           <p class="title-12 mt-4">February 2023&nbsp; {{ typeof active_account}}</p>
                            <i class="isax isax-calendar-circle5 fs-4 text-blue mt-4" style="bottom: 4px;"></i>
                         </div>
                      </div>
                   </div>
-                  <canvas id="lineChart" class="bg-white"></canvas>
+                  <canvas v-if="active_account" id="lineChart" class="bg-white"></canvas> <!--active account-->
+                  <div v-else class="d-flex flex-row justify-content-center" style="background-color: white; height: 400px;"> <!--not active-->
+                     <div class="d=flex flex-column justify-content-center text-center my-auto ">
+                           <img src="/src/assets/images/Rectangle.png" >
+                           <div class="title-7 text-purple pt-2">Activate Account</div>
+                           <p class="title-12 pt-2">Accessing this feature requires the completion of KYC process</p>
+                           <div class="pt-2">
+                                 <button type="button" class="button-hover-icon-purple bg-purple border-0 text-white" >
+                                 <span class="title-8 translane-middle-y text-nowrap mx-2 align-text-bottom"> Activate Account </span>
+                                 </button>
+                           </div>
+                     </div>
+                  </div>
+                   <canvas id="myChart"></canvas>
                </div>
             </div>
          </div>
@@ -115,11 +151,13 @@
                      <div class="col-6 justify-content-start d-flex">
                         <p class="title-6 mx-2 mt-2 mb-2">Gross Revenue</p>
                      </div>
-                     <div class="col-6 justify-content-end d-flex mt-3">
-                        <span><i class="isax isax-refresh text-purple fs-5 me-3"></i></span>
+                     <div class="col-6 justify-content-end d-flex my-auto">
+                        <span class="me-2">
+                        <img src="/src/assets/images/sync.svg">
+                        </span>
                      </div>
                   </div>
-                   <div class="d-flex justify-content-center mt-3">
+                   <div class="d-flex justify-content-center mt-3" >
                      <p class="title-8 pe-2"><span class="badge rounded-pill bg-purple mx-1">&nbsp;</span> GCash</p>
                      <p class="title-8 pe-2"><span class="badge rounded-pill bg-black mx-1">&nbsp;</span> BPI</p>
                      <p class="title-8 pe-2"><span class="badge rounded-pill bg-blue mx-1">&nbsp;</span> Maya</p>
@@ -134,23 +172,32 @@
                            <div class="d-flex mt-2">
                               <p class="title-12">February 2023&nbsp;</p>
                               <i class="isax isax-calendar-circle5 fs-4 text-blue mx-1" style="bottom: 4px;"></i>
-                           </div>
+                           </div> 
                         </div>
                      </div>
                   </div>
-                  <canvas id="barChart" class="bg-white"></canvas>
+
+                   <canvas v-if="active_account" id="barChart" class="bg-white"></canvas> <!--revenue_account-->
+                   <div v-else class="d-flex flex-row justify-content-center" style="background-color: white; height: 260px;">
+                     <div class="d=flex flex-column justify-content-center text-center my-auto ">
+                           <div class="title-7 text-purple pt-2">Activate Account</div>
+                           <p class="title-12 pt-2">Accessing this feature requires the completion of KYC process</p>
+                     </div>
+                   </div> 
                </div>
 
                </div>
-               <div class="col-12 col-lg-6 ">
+               <div  v-if="active_account" class="col-12 col-lg-6 ">
                   <div class=" bg-white overflow-hidden ms-2">
-                  <div class="row gx-0 g-0 border-bottom">
-                     <div class="col justify-content-start d-flex">
-                        <p class="title-6 mx-3 mt-2 mb-2">Channel Ranking</p>
-                     </div>
-                     <div class="col-6 justify-content-end d-flex mt-3">
-                        <span><i class="isax isax-refresh text-purple fs-5 me-3"></i></span>
-                     </div>
+                     <div class="row gx-0 g-0 border-bottom">
+                        <div class="col justify-content-start d-flex">
+                         <p class="title-6 mx-3 mt-2 mb-2">Channel Ranking</p>
+                        </div>
+                        <div class="col-6 justify-content-end d-flex my-auto">
+                             <span class="text-purple me-3">
+                                <img src="/src/assets/images/sync.svg">
+                             </span>
+                        </div>
                   </div>
                   <div class="col-12 mt-3 mx-4">
                      <div class="row">
@@ -246,25 +293,55 @@
                </div>
 
                </div>
-               
+               <div v-if="!active_account" class="col-12 col-lg-6 ">
+                  <div class=" bg-white overflow-hidden ms-2">
+                     <div class="row gx-0 g-0 border-bottom">
+                        <div class="col justify-content-start d-flex">
+                         <p class="title-6 mx-3 mt-2 mb-2">Channel Ranking</p></div>
+                        <div class="col-6 justify-content-end d-flex my-auto">
+                             <span class="text-purple me-3">
+                                <img src="/src/assets/images/sync.svg">
+                             </span>
+                        </div>
+                     </div>
+
+                  <div class="col-12 mt-1 mx-4">
+                     <div class="row">
+                        <canvas class="bg-white"></canvas> <!--channel_ranking-->
+                        <div class="d-flex flex-row justify-content-center" style="background-color: white; height: 210px;">
+                           <div class="d=flex flex-column justify-content-center text-center my-auto mt-5 ">
+                              <div class="title-7 text-purple pt-2">Activate Account</div>
+                              <p class="title-12 pt-2">Accessing this feature requires the completion of KYC process</p>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+
+               </div>
+
             </div>
          </div>
       </div>
    </div>
+
+   
 </template>
 <script>
-   import Chart from "chart.js/auto";
-   
-   export default {
-     data() {
+   import Chart from "chart.js/auto" ;
+
+    export default {
+      data() {
        return {
+         active_account: false,
+        
          durations: [
-           {
-             name: "[Daily, Weekly, Monthly, Yearly]",
-           },
-         ],
-       };
-     },
+        {
+           name: "[Daily, Weekly, Monthly, Yearly]",
+            },
+          ],
+        };
+      },
    
      export_reports_modal: false,
      mounted() {
@@ -387,7 +464,7 @@
            },
          },
        };
-   
+      
        new Chart(document.getElementById("lineChart"), lineConfig);
        new Chart(document.getElementById("barChart"), barConfig);
      },
